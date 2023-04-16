ca2f544943b585880ddc76dcab2c54c0ad68967d
furu0202 <furuka.0202@gmail.com>
2023-04-17 02:33:41 +0900

    【4月16日】問題4・問題5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1【4月16日】変数xとyに数値を代入し、その和を表示するプログラムを作成してください。
+// 【4月16日】変数xとyに数値を代入し、その和を表示するプログラムを作成してください。
 // let x = 5;
 // let y = 10;
 // let sum = x + y;
@@ -10,7 +10,7 @@ const y = 7;
 const z = x + y;
 console.log(z);
 
-// 2【4月16日】forループを使用して、1から10までの整数をすべて表示するプログラムを作成してください。
+// 【4月16日】forループを使用して、1から10までの整数をすべて表示するプログラムを作成してください。
 // 【？】forループが何か分からない。
 // 【・】i < 10 にしていたので１から９までしか出力されなかった。
 // for (let i = 1; i <= 10; i++) {
@@ -21,7 +21,7 @@ for (i = 1; i <= 10; i++) {
   console.log(i);
 }
 
-// 3【4月16日】 配列numbersに10個の整数が格納されているとします。配列numbersのすべての要素を2倍にして、
+// 【4月16日】 配列numbersに10個の整数が格納されているとします。配列numbersのすべての要素を2倍にして、
 // 新しい配列doubledNumbersに格納するプログラムを作成してください。
 // 【？】配列を2倍にする方法が分からない
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -36,3 +36,33 @@ const doubleNumbers = numbers.map(function (numbers) {
   return numbers * 2;
 });
 console.log(doubleNumbers);
+
+// 【4月16日】関数multiplyを作成してください。
+// この関数は、2つの数値を引数として受け取り、それらの積を返すものです。
+// 【？】２つの数値を引数として受け取り、積を返す方法 (戻り値)
+// function multiply(num1, num2) {
+//     return num1 * num2;
+//   }
+//   const result = multiply(5, 6);
+// console.log(result); // 結果:30
+
+function multiply(num1, num2) {
+  return 10 * 11;
+}
+const answer = multiply(10, 11);
+console.log(answer);
+
+// 【4月16日】オブジェクトpersonがあり、そのプロパティとしてnameとageが定義されています。
+// オブジェクトpersonのageプロパティの値を1増やすプログラムを作成してください。
+// 【？】プロパティの値を１増やすプログラムの書き方
+// 【・】const person定義は問題なく出来たが、person.age++;で苦戦を強いられた
+// let person = {
+//     name: "John",
+//     age: 30
+//   };
+//   person.age++;
+// console.log(person);
+
+const person = { name: 'furu', age: 26 };
+person.age += 1;
+console.log(person);
