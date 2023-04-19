ebdfade47dfd292e9b31b21c19cd53727f71d11b
furu0202 <furuka.0202@gmail.com>
2023-04-19 23:57:41 +0900

    【4月19日】問題1

---
+++ 0419.js
@@ -0,0 +1,11 @@
+// prompt関数を使って、ユーザーに好きな数字を入力してもらい、
+// その数字が偶数か奇数かを判定し、コンソールに出力するプログラムを書いてください。
+// 【・】問題の意図(prompt関数）が全く理解できない。
+// let inputNumber = prompt("好きな数字を入力してください");
+// let number = parseInt(inputNumber); // 文字列を数値に変換
+
+// if (number % 2 === 0) {
+//   console.log(number + "は偶数です");
+// } else {
+//   console.log(number + "は奇数です");
+// }
