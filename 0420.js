5ff1fb47f0890ffba30b76b52fd2d8f49923a53e
furu0202 <furuka.0202@gmail.com>
2023-04-20 23:56:58 +0900

    【4月20日】問題1・問題2

---
+++ 0420.js
@@ -0,0 +1,28 @@
+// 変数を宣言し、数値を代入して、その値を2倍にしてコンソールに出力するプログラムを書いてください。
+// 【・】正解
+// let num = 5;
+// let doubledNum = num * 2;
+// console.log(doubledNum);
+
+const number = 6;
+const getNumber = number * 2;
+console.log(getNumber);
+
+// 条件分岐を使って、変数の値が正の数、負の数、0の場合で、コンソールにメッセージを出力するプログラムを書いてください。
+// 【・】if文、不等号の使い方がわからない
+// if(num > 0){
+//     console.log("numは正の数です");
+//   } else if(num === 0){
+//     console.log("numは0です");
+//   } else {
+//     console.log("numは負の数です");
+//   }
+const num = 7;
+if (num > 0) {
+  console.log('numは正の数');
+} else if (num === 0) {
+  console.log('numは0');
+} else if (num < 0);
+{
+  console.log('numは負の数');
+}
