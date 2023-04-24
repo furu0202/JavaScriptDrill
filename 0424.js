94d3c85b2c5edba0aa9daae70ba3df99955689ac
furu0202 <furuka.0202@gmail.com>
2023-04-24 23:59:12 +0900

    【4月24日】問題1・問題2

---
+++ 0424.js
@@ -0,0 +1,32 @@
+// 2つの数値を引数として受け取り、その2つの数値のうち、大きい方の値をコンソールに出力するプログラムを書いてください。
+// 【・】数値を比較して大きい方の値をコンソールする方法が分からない。
+// function getMax(num1, num2) {
+//     if (num1 > num2) {
+//       console.log(num1);
+//     } else {
+//       console.log(num2);
+//     }
+//   }
+//   getMax(10, 5); /
+
+function getNumber(num1,num2);
+if (num1 < num2);
+console.log(""
+)
+
+// 変数を宣言し、0から9までのランダムな整数値を代入して、
+// その値が偶数か奇数かを判定し、偶数の場合は"偶数です"、奇数の場合は"奇数です"とコンソールに出力するプログラムを書いてください。
+// 【・】ランダムな整数値の代入方法、奇数偶数の判定方法が理解できなかった
+// let num = Math.floor(Math.random() * 10);
+// if (num % 2 === 0) {
+//   console.log(num + "は偶数です");
+// } else {
+//   console.log(num + "は奇数です");
+// }
+
+const number = 6;
+if (number / 2 === 0){
+    console.log("偶数です");
+} else {
+    console.log("奇数です");
+}
