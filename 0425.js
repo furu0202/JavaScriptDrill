b756dd0f0d57049b4e44975454e283802db92dc2
furu0202 <furuka.0202@gmail.com>
2023-04-25 23:58:38 +0900

    【4月25日】問題1・問題2

---
+++ 0425.js
@@ -0,0 +1,15 @@
+// 変数を宣言し、自分の名前を代入して、その名前をコンソールに出力するプログラムを書いてください。
+// 【・】正解
+const myName = 'furu';
+console.log(myName);
+
+// 配列を使って、5つの文字列を格納し、逆順で配列の全ての要素をコンソールに出力するプログラムを書いてください。
+// 【・】調べながら実践してみた。
+// const strings = ["apple", "banana", "cherry", "date", "elderberry"];
+
+// for (let i = strings.length - 1; i >= 0; i--) {
+//   console.log(strings[i]);
+// }
+let fiveWozrds = ['は', 'ひ', 'ふ', 'へ', 'ほ'];
+fiveWords = fiveWords.reverse();
+console.log(fiveWords);
