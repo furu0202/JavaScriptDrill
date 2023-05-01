f6e1c10357586940ae529ed28888f79098f70759
furu0202 <furuka.0202@gmail.com>
2023-05-01 23:56:34 +0900

    【4月30日】問題1・問題2

---
+++ 0430.js
@@ -0,0 +1,12 @@
+// 変数を宣言し、2つの数値を代入して、その2つの数値を足した結果をコンソールに出力するプログラムを書いてください。
+// let num1 = 10;
+// let num2 = 5;
+// let sum = num1 + num2;
+// console.log(sum);
+const number1 = 11;
+const number2 = 18;
+console.log(number1 + number2);
+
+// 変数を宣言し、文字列を代入して、その文字列の長さをコンソールに出力するプログラムを書いてください。
+const word = 'イングリッシュ';
+console.log(word.length);
