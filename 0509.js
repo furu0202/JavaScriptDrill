e1dc1bb3e005698dc030510101029d6b98ddf149
furu0202 <furuka.0202@gmail.com>
2023-05-09 23:58:09 +0900

    【5月9日】問題1〜問題3

---
+++ 0509.js
@@ -0,0 +1,14 @@
+// 変数xに数値5を代入し、変数yに数値3を代入し、xとyの積をコンソールに出力するプログラムを作成してください。
+const x = 5;
+const y = 3;
+console.log(x * y);
+
+// 文字列「Everyday!」をコンソールに出力するプログラムを作成してください。
+const word = 'Everyday!';
+console.log(word);
+
+// オブジェクトpersonがあり、そのプロパティとしてnameとageとbirthdayが定義されています。
+// オブジェクトpersonのageプロパティの値を2倍にするプログラムを作成してください。
+const person = { name: 'Hikari', age: 16, birthday: '2006-09-28' };
+person.age = person.age * 2;
+console.log(person);
