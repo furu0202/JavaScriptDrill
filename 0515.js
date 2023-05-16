9f8e8a4a5a51822263cce092ee61b2ded85635db
furu0202 <furuka.0202@gmail.com>
2023-05-16 23:54:43 +0900

    【5月15日】問題3〜問題5

--- 0515.js
+++ 0515.js
@@ -15,3 +15,28 @@ if (score >= 90) {
 } else {
   console.log('不合格');
 }
+
+// 関数addを定義し、引数xとyを受け取り、その和を返すプログラムを作成してください。
+// また、関数を使用して、数値を加算した結果をコンソールに出力するプログラムを作成してください。
+function add(x, y) {
+  return x + y;
+}
+const result = add(4, 5);
+console.log(result);
+
+// 変数を宣言し、2つの数値を代入して、その2つの数値を比較して、
+// 小さい方の数値をコンソールに出力するプログラムを書いてください。
+
+const number1 = 1;
+const number2 = 2;
+
+if (number1 < number2) {
+  console.log(number1);
+} else {
+  console.log(number2);
+}
+
+// 変数を宣言し、適当な文字列を2つ代入して、それぞれの値をコンソールに出力するプログラムを書いてください。
+const word1 = 'えび';
+const word2 = 'シャケ';
+console.log(word1, word2);
